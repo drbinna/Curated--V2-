d20b6c6a4edb8dac291f4ad3f3f34a5c329ed6a7
DAVID <davidopara2019@gmail.com>
2025-11-16 00:13:46 +0100

    new_update

--- config/cors.php
+++ config/cors.php
@@ -3,10 +3,7 @@
 return [
     'paths' => ['*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [
-        'https://plot-oculus-75994912.figma.site',
-        'https://figma.com',
-    ],
+    'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => ['*'],
